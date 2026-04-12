b2736d4a02d96c436b7c65e0ce644044adde9096
Malo Legendre-Lemaire <malolegendrelemaire@gmail.com>
2026-04-12 11:28:39 +0200

    feat: ?no-ssr to disable ssr

--- examples/testsuit/src/entry-server.ts
+++ examples/testsuit/src/entry-server.ts
@@ -1,8 +1,10 @@
+import type { Connect } from "vite";
 import App from "./app";
 import { renderToString } from "@lentjs/core";
 
-export function render(_req: Request): string {
-  if (false as unknown)
+export function render(req: Connect.IncomingMessage): string {
+  const url = new URL(`http://localhost${req.url}`);
+  if (url.searchParams.get("no-ssr") != null)
     return "";
   try {
     return renderToString(App);
