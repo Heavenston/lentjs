0e6e9625a573924af61b9fa290315df4094f739d
Malo Legendre-Lemaire <malolegendrelemaire@gmail.com>
2026-04-16 10:19:00 +0200

    wip: Making a new Scope class to replace owner and root

---
+++ packages/core-reactivity/src/scope.ts
@@ -0,0 +1,169 @@
+import { noop, remove } from "@lentjs/utils";
+
+export type ScopeState = "alive" | "detached" | "cleaned";
+export type FinalScopeStates = Exclude<ScopeState, "alive">;
+export type ScopeUnsubscribe = () => void;
+declare const ContextKeySymbol: unique symbol;
+/**
+ * Opaque type representing the key for a context value
+ */
+export type ContextKey<T> = { [ContextKeySymbol]: T };
+
+export type ScopeCleanup = {
+  scope: Scope;
+  (): void;
+  detach(): void;
+};
+
+export function createContextKey<T>(id: string): ContextKey<T> {
+  // @ts-ignore Fake convertion
+  return id;
+}
+
+export class Scope {
+  static #currentScope: Scope | null = null;
+
+  public static get currentScope(): Scope | null {
+    return Scope.#currentScope;
+  }
+
+  public static create(parent: Scope | null = Scope.currentScope): Scope {
+    const scope = new Scope(parent);
+    if (parent) {
+      scope.#cleanupWithParent();
+      scope.#detachWithParent();
+    }
+    return scope;
+  }
+
+  public static createControlled(parent: Scope | null = Scope.currentScope): [scope: Scope, cleanup: ScopeCleanup] {
+    const scope = new Scope(parent);
+    if (parent)
+      scope.#cleanupWithParent();
+
+    const cleanup: ScopeCleanup = () => {
+      scope.#setState("cleaned");
+    };
+    cleanup.scope = scope;
+    cleanup.detach = () => {
+      scope.#setState("detached");
+    };
+
+    return [scope, cleanup];
+  }
+
+  #state: ScopeState = "alive";
+  #detachingWithParent: boolean = false;
+  #contextValues: Map<unknown, unknown> = new Map;
+  readonly #callbacks: Record<FinalScopeStates, (() => void)[]> = {
+    cleaned: [],
+    detached: [],
+  };
+  public readonly parent: Scope | null;
+
+  public get state(): ScopeState {
+    return this.#state;
+  }
+
+  private constructor(parent: Scope | null) {
+    this.parent = parent;
+  }
+
+  #setState(newState: FinalScopeStates) {
+    if (this.state !== "alive") {
+      if (this.state !== newState) {
+        console.warn(`Scope in state ${this.state} cannot switch to ${newState}`);
+      }
+      return;
+    }
+
+    this.#callbacks[newState].splice(0).forEach(cb => cb());
+    // Remove the other callbacks, they are not needed anymore
+    this.#callbacks[newState === "cleaned" ? "detached" : "cleaned"].splice(0);
+  }
+
+  #cleanupWithParent() {
+    const parent = this.parent!;
+    const unsub = parent.on("cleaned", () => this.#setState("cleaned"));
+    this.on("cleaned", unsub);
+    this.on("detached", unsub);
+  }
+
+  #detachWithParent() {
+    this.#detachingWithParent = true;
+    const parent = this.parent!;
+    const unsub = parent.on("detached", () => this.#setState("detached"));
+    this.on("cleaned", unsub);
+    this.on("detached", unsub);
+  }
+
+  public enter<T, A extends any[]>(cb: (...args: A) => T, ...args: A): T {
+    const prev = Scope.#currentScope;
+    Scope.#currentScope = this;
+    try {
+      return cb(...args);
+    }
+    catch(e) {
+      throw e;
+    }
+    finally {
+      Scope.#currentScope = prev;
+    }
+  }
+
+  public on(state: FinalScopeStates, cb: () => void): ScopeUnsubscribe {
+    if (this.state === state) {
+      cb();
+      return noop;
+    }
+    if (this.state !== "alive") {
+      console.warn(`Cannot listen for scope state ${state} from state ${this.state}`);
+      return noop;
+    }
+    this.#callbacks[state].push(cb);
+    return () => {
+      remove(this.#callbacks[state], cb);
+    };
+  }
+
+  public onCleanup(cb: () => void): ScopeUnsubscribe {
+    return this.on("cleaned", cb);
+  }
+
+  public onDetach(cb: () => void): ScopeUnsubscribe {
+    return this.on("detached", cb);
+  }
+
+  public hasContext<T>(key: ContextKey<T>): boolean {
+    return this.#contextValues.has(key);
+  }
+
+  public getContext<T>(key: ContextKey<T>): T {
+    if (!this.#contextValues.has(key)) {
+      if (this.parent === null)
+        throw new Error(`No context value found for key ${key}`);
+      return this.parent.getContext(key);
+    }
+    return this.#contextValues.get(key) as T;
+  }
+
+  public tryGetContext<T>(key: ContextKey<T>): T | null {
+    if (!this.#contextValues.has(key)) {
+      if (this.parent === null)
+        return null;
+      return this.parent.tryGetContext(key);
+    }
+    return this.#contextValues.get(key) as T;
+  }
+
+  public setContext<T>(key: ContextKey<T>, value: T) {
+    this.#contextValues.set(key, value);
+  }
+}
+
+export function getScope(): Scope {
+  const scope = Scope.currentScope;
+  if (scope === null)
+    throw new Error("getScope: No current scope");
+  return scope;
+}
