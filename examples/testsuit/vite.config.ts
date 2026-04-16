9f0e6c99f58d18a998ff329db9caae7a8937fe25
Malo Legendre-Lemaire <malolegendrelemaire@gmail.com>
2026-04-16 15:26:50 +0200

    feat: Moved the lent-id plugin from the example vite config

--- examples/testsuit/vite.config.ts
+++ examples/testsuit/vite.config.ts
@@ -52,25 +52,6 @@ export default defineConfig({
   plugins: [
     LentJS(),
     (() => ({
-      name: "lent-id",
-      transform: {
-        filter: {
-          code: /____RANDOM_ID/,
-        },
-        async handler(code, id) {
-          function toBase64(arr: Uint8Array): string {
-            return (arr as any).toBase64();
-          }
-          const prefix = toBase64(new Uint8Array(await crypto.subtle.digest("SHA-256", Uint8Array.from(`${id}\n${code}`)))).replace(/[+/=]/g, "").slice(0,16);
-          let i = 0;
-          const newId = () => i++;
-          return {
-            code: code.replace(/____RANDOM_ID/g, () => `${prefix}_${newId().toString(16).padStart(2, "0")}`),
-          };
-        },
-      },
-    }) satisfies Plugin)(),
-    (() => ({
       name: "lent-ssr",
       enforce: "pre",
 
