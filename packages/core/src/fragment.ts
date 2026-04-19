2fee47448cd20d101a7fd6ee28c7a90c90f45c55
Malo Legendre-Lemaire <malolegendrelemaire@gmail.com>
2026-04-19 19:36:17 +0200

    added explanation comment in fragment

--- packages/core/src/fragment.ts
+++ packages/core/src/fragment.ts
@@ -3,5 +3,7 @@ import type { JSXElement } from ".";
 import { register } from "@lentjs/core-serialize";
 
 export const Fragment = register((props: { children?: JSXElement }): JSXElement => {
+  // equivalent to:
+  // return () => props.children;
   return (getProperty<{ children?: JSXElement }, "children">).bind(null, props, "children");
 }, "__lentjs_fragment");
