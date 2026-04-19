7be3cbca14f6a8fedc57e194064ee545c5959ae1
Malo Legendre-Lemaire <malolegendrelemaire@gmail.com>
2026-04-19 11:56:18 +0200

    refact: Moved lentjs-ssr plugin into vitejs-plugin package

--- examples/testsuit/vite.config.ts
+++ examples/testsuit/vite.config.ts
@@ -1,46 +1,5 @@
-import { type Plugin, defineConfig, type Connect } from 'vite'
-import * as fs from "fs/promises";
+import { defineConfig } from 'vite'
 import LentJS from "@lentjs/vitejs-plugin";
-
-const shouldSsrRender = (req: Connect.IncomingMessage) => {
-  const pathname = req.url ?? "";
-  if (/\.[\w?=&]+$/.test(pathname) && !pathname.endsWith('.html')) {
-    // has extension
-    return false;
-  }
-  if (pathname.includes('_-vite-ping')) {
-    return false;
-  }
-  if (pathname.includes('__open-in-editor')) {
-    return false;
-  }
-  if (pathname.includes('?editor:')) {
-    return false;
-  }
-  // if (url.searchParams.has('html-proxy')) {
-  //   return false;
-  // }
-  // if (url.searchParams.get('ssr') === 'false') {
-  //   return false;
-  // }
-  if (pathname.includes('@builder.io/qwik/build')) {
-    return false;
-  }
-  if (pathname.includes('@vite')) {
-    return false;
-  }
-  const acceptHeader = req.headers.accept || '';
-  const accepts = acceptHeader.split(',').map((accept) => accept.split(';')[0]);
-  if (accepts.length == 1 && accepts.includes('*/*')) {
-    // special case for curl where the default is `*/*` with no additional headers
-    return true;
-  }
-
-  if (!accepts.includes('text/html')) {
-    return false;
-  }
-  return true;
-};
 
 export default defineConfig({
   server: {
@@ -49,42 +8,5 @@ export default defineConfig({
 
   oxc: false,
 
-  plugins: [
-    LentJS(),
-    (() => ({
-      name: "lent-ssr",
-      enforce: "pre",
-
-      configureServer: (server) => {
-        server.middlewares.use(async (req, res, next) => {
-          if (!shouldSsrRender(req))
-            return next();
-
-          try {
-            console.log("MIDDLEWARE");
-
-            res.setHeader('Content-Type', 'text/html; charset=utf-8');
-            res.setHeader('Cache-Control', 'no-cache, no-store, max-age=0');
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('X-Powered-By', 'Qwik Vite Dev Server');
-            res.writeHead(200);
-
-            let html = await fs.readFile("./index.html", { encoding: "utf-8" });
-            html = await server.transformIndexHtml(req.url!, html);
-
-            const { render } = await server.ssrLoadModule("src/entry-server.ts");
-
-            html = html.replace("<!--ssr-outlet-->", render(req));
-
-            res.write(html);
-            res.end();
-          }
-          catch(e) {
-            server.ssrFixStacktrace(e);
-            console.error(e);
-          }
-        });
-      },
-    }) satisfies Plugin)(),
-  ],
+  plugins: [LentJS()],
 });
--- packages/vitejs-plugin/src/index.ts
+++ packages/vitejs-plugin/src/index.ts
@@ -1,14 +1,16 @@
 import { type PluginOption } from "vite";
 import { compilePlugin } from "./compile-plugin.js";
 import { randomIdPlugin } from "./random-id-plugin.js";
+import { ssrPlugin } from "./ssr-plugin.js";
 
 export type LentjsVitejsPluginConfig = {
-  
+  disableSSR?: boolean,
 };
 
-export default function LentjsVitejsPlugin(_cfg: LentjsVitejsPluginConfig = {}): PluginOption {
+export default function LentjsVitejsPlugin(cfg: LentjsVitejsPluginConfig = {}): PluginOption {
   return [
     compilePlugin(),
     randomIdPlugin(),
+    cfg.disableSSR ? [] : [ssrPlugin()],
   ];
 };
